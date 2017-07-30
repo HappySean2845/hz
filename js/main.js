35fa1caaf4bd968d4325139898b97ae858d33e2d
jiangjie404 <404877757@qq.com>
2017-07-31 01:21:29 +0800

    jj

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 /**
  * Created by Darre on 17/07/2017.
    */
-alert('jj335')
-
   //公用popup方法   popup用来制定popupcontainer中指定的某个popup   popkeypopu的id
 var popupShow = function (popkey,speed){
   if(typeof(speed) != "number") speed = 0;
